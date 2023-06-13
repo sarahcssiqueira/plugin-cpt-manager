5117e9e449e426a3fd405f2ed93598ef5c74512d
sarahcssiqueira <sarahcso@hotmail.com>
2023-06-13 13:35:34 -0300

    refactor: completely rewrite to improve code

--- custom-posts-register.php
+++ custom-posts-register.php
@@ -1,76 +1,74 @@
 <?php
 /**
-* Plugin Name:       Custom Post Register
-* Plugin URI:        https://sarahjobs.com/wordpress/plugins/cpt-register
-* Description:       Register custom posts types for your project
-* Version:           1.0.0
-* Requires at least: 5.4
-* Requires PHP:      7.4
-* Author:            Sarah Siqueira
-* Author URI:        https://sarahjobs.com/about
-* License:           GPLv2 or later
-* License URI:       https://www.gnu.org/licenses/gpl.html
-* Text Domain:       cpt-register
-* Domain Path:       /languages
-* Update URI:        https://sarahjobs.com/wordpress/plugins/cpt-register/update
-*/
+ * Plugin Name:       Custom Post Register
+ * Plugin URI:        https://sarahjobs.com/wordpress/plugins/cpt-register
+ * Description:       Register custom posts types for your project
+ * Version:           1.0.0
+ * Requires at least: 5.4
+ * Requires PHP:      7.4
+ * Author:            Sarah Siqueira
+ * Author URI:        https://sarahjobs.com/about
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl.html
+ * Text Domain:       cpt-register
+ * Domain Path:       /languages
+ * Update URI:        https://sarahjobs.com/wordpress/plugins/cpt-register/update
+ */
 
+/**
+ * Exit if accessed directly 
+ **/
+if(! defined('ABSPATH') ) { exit;
+}
 
-    function new_posttype_pet() {
+new CustomPostsRegister(); // Initialize
 
-    // Core feature(s) the post type supports. Default is an array containing 'title' and 'editor'.
+class CustomPostsRegister
+{
 
-        $supports = array(
-        'title',
-        'editor',
-        'author', 
-        'thumbnail', 
-        'excerpt', 
-        'custom-fields', // support custom fields
-        'comments', 
-        'revisions', 
-        'post-formats', 
+    function __construct()
+    {    
+        add_action('init', array($this, 'register_cpt'));
+    }
+
+    function register_cpt()
+    {
+        $cpt_name = 'Cpt';
+
+        register_post_type(
+            '$cpt_name', array(
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'author', 
+                    'thumbnail', 
+                    'excerpt', 
+                    'custom-fields', 
+                    'comments', 
+                    'revisions', 
+                    'post-formats', 
+                ),
+                'labels' => array(
+                    'name' => _x($cpt_name, 'plural'),
+                    'singular_name' => _x($cpt_name, 'singular'),
+                    'menu_name' => _x($cpt_name, 'admin menu'),
+                    'name_admin_bar' => _x($cpt_name, 'admin bar'),
+                    'add_new' => _x('Add' . ' ' . $cpt_name, 'add new' . $cpt_name),
+                    'add_new_item' => __('Add New' . ' ' . $cpt_name),
+                    'new_item' => __('New' . ' ' . $cpt_name, 'New' . ' ' . $cpt_name),
+                    'edit_item' => __('Edit' . ' ' . $cpt_name),
+                    'view_item' => __('View' . ' ' . $cpt_name),
+                    'all_items' => __('All' . ' ' . $cpt_name),
+                    'search_items' => __('Search' . $cpt_name),
+                    'not_found' => __('No' . ' ' .$cpt_name . ' found'),
+                ),
+                'public' => true,
+                'query_var' => true,
+                'rewrite' => array('slug' => $cpt_name),
+                'has_archive' => true,
+                'hierarchical' => false,
+                )
         );
-        
+    }
 
-    //Array of labels for this post type.
-
-        $labels = array(
-        'name' => _x('pets', 'plural'),
-        'singular_name' => _x('pet', 'singular'),
-        'menu_name' => _x('Pets', 'admin menu'),
-        'name_admin_bar' => _x('Pet', 'admin bar'),
-        'add_new' => _x('Add Pet', 'add new pet'),
-        'add_new_item' => __('Add New Pet'),
-        'new_item' => __('New pet', 'New Pet'),
-        'edit_item' => __('Edit pet'),
-        'view_item' => __('View pet'),
-        'all_items' => __('All pets'),
-        'search_items' => __('Search pets'),
-        'not_found' => __('No pets found.'),
-        );
-
-
-    // Defining Arguments for Custom Post Type
-        
-        $args = array(
-        'supports' => $supports,
-        'labels' => $labels,
-        'public' => true,
-        'query_var' => true,
-        'rewrite' => array('slug' => 'pets'),
-        'has_archive' => true,
-        'hierarchical' => false,
-        );
-
-        
-        register_post_type('pets', $args);
-        }
-
-
-        add_action('init', 'new_posttype_pet');
-
-
-
-      //  More information about this function on official documentation: 
-      // 'https://developer.wordpress.org/plugins/post-types/registering-custom-post-types/' */
+}
