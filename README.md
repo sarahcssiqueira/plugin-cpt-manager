c8c4e03ed5c6c1b6e880a5d8ffae7567cf837ab3
sarahcssiqueira <sarahcso@hotmail.com>
2023-08-21 12:27:31 -0300

    feat: added composer to handle with WordPress Codgin Standards
    doc: improve documentation with comments and readme.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Plugin Custom Post Types Manager
+# Custom Post Types Manager
+
+[![Project Status: Active – The project has reached a stable, usable state and is being actively developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
+[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+[![Release Version](https://img.shields.io/github/release/sarahcssiqueira/plugin-order-status-admin.svg)](https://github.com/sarahcssiqueira/plugin-order-status-admin/releases/latest)
+[![Support Level](https://img.shields.io/badge/support-may_take_time-yellow.svg)](#support-level)
 
 WordPress Core provides five post types by default: Post, Page, Attachment, Revision and Menu. Although, while working on a project, we may need to create our specific content types, and that can be easily done with a few lines of code. That is the purpose of the main Class in this plugin, using the [**register_post_type()**](https://developer.wordpress.org/reference/functions/register_post_type/) function.
 
--- custom-posts-register.php
+++ custom-posts-register.php
@@ -13,87 +13,101 @@
  * Text Domain:       cpt-manager
  * Domain Path:       /languages
  * Update URI:        https://sarahjobs.com/wordpress/plugins/cpt-manager/update
+ *
+ * @package CustomPostTypesManager
+ * @author sarahcssiqueira
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
 new CustomPostsManager();
 
+/**
+ * Main Class
+ */
 class CustomPostsManager {
 
-    public $custom_post_types = array();
+	public $custom_post_types = [];
 
+	/**
+	 * Constructor for handle WordPress Hooks
+	 */
 	public function __construct() {
-        
-        add_action( 'init', array( $this, 'post_types_list' ) );
-		add_action( 'init', array( $this, 'register_cpt' ) );
+
+		add_action( 'init', [ $this, 'post_types_list' ] );
+		add_action( 'init', [ $this, 'register_cpt' ] );
 	}
 
-    public function post_types_list() {
+	/**
+	 * Store the custom post types inside an
+	 * array
+	 */
+	public function post_types_list() {
 
-		$this->custom_post_types = array(
-			array(
-				'name'  => 'projects',
+		$this->custom_post_types = [
+			[
+				'name'      => 'projects',
 				'singular'  => 'project',
-				'label' => 'My Projects',
-                'menu_icon'    => 'dashicons-media-document',
-			),
-			array(
-				'name'  => 'Ideas',
+				'label'     => 'My Projects',
+				'menu_icon' => 'dashicons-media-document',
+			],
+			[
+				'name'      => 'Ideas',
 				'singular'  => 'ideas',
-				'label' => 'My Ideas',
-                'menu_icon'    => 'dashicons-art',
-			),
-		);
+				'label'     => 'My Ideas',
+				'menu_icon' => 'dashicons-art',
+			],
+		];
 
 	}
 
+	/**
+	 * Register the post types
+	 */
 	public function register_cpt() {
 
-        foreach ( $this->custom_post_types as $custom_post_type)  { 
+		foreach ( $this->custom_post_types as $custom_post_type ) {
 
-		register_post_type(
-			$custom_post_type['name'],
-			array(
-				'supports'     => array(
-					'title',
-					'editor',
-					'author',
-					'thumbnail',
-					'excerpt',
-					'custom-fields',
-					'comments',
-					'revisions',
-					'post-formats',
-				),
-				'labels'       => array(
-					'name'           => _x( $custom_post_type['label'], 'plural' ),
-					'singular_name'  => _x( $custom_post_type['singular'], 'singular' ),
-					'menu_name'      => _x( $custom_post_type['label'], 'admin menu' ),
-					'name_admin_bar' => _x( $custom_post_type['label'], 'admin bar' ),
-					'add_new'        => _x( 'Add ' . $custom_post_type['singular'], 'add new project' ),
-					'add_new_item'   => __( 'Add ' . $custom_post_type['singular'] ),
-					'new_item'       => __( 'New ' . $custom_post_type['singular'], 'New project' ),
-					'edit_item'      => __( 'Edit ' . $custom_post_type['singular'] ),
-					'view_item'      => __( 'View ' . $custom_post_type['singular'] ),
-					'all_items'      => __( 'All ' . $custom_post_type['name'] ),
-					'search_items'   => __( 'Search ' . $custom_post_type['name']),
-					'not_found'      => __( 'No ' . $custom_post_type['name'] . ' found' ),
-				),
-				'public'       => true,
-                'show_in_rest' => true,
-				'query_var'    => true,
-				'rewrite'      => array(
-					'slug' => $custom_post_type['singular'],
-				),
-				'has_archive'  => true,
-				'hierarchical' => false,
-				'menu_icon'    => $custom_post_type['menu_icon'],
-			)
-		);
+			register_post_type(
+				$custom_post_type['name'],
+				[
+					'supports'     => [
+						'title',
+						'editor',
+						'author',
+						'thumbnail',
+						'excerpt',
+						'custom-fields',
+						'comments',
+						'revisions',
+						'post-formats',
+					],
+					'labels'       => [
+						'name'           => _x( $custom_post_type['label'], 'plural' ),
+						'singular_name'  => _x( $custom_post_type['singular'], 'singular' ),
+						'menu_name'      => _x( $custom_post_type['label'], 'admin menu' ),
+						'name_admin_bar' => _x( $custom_post_type['label'], 'admin bar' ),
+						'add_new'        => _x( 'Add ' . $custom_post_type['singular'], 'add new project' ),
+						'add_new_item'   => __( 'Add ' . $custom_post_type['singular'] ),
+						'new_item'       => __( 'New ' . $custom_post_type['singular'], 'New project' ),
+						'edit_item'      => __( 'Edit ' . $custom_post_type['singular'] ),
+						'view_item'      => __( 'View ' . $custom_post_type['singular'] ),
+						'all_items'      => __( 'All ' . $custom_post_type['name'] ),
+						'search_items'   => __( 'Search ' . $custom_post_type['name'] ),
+						'not_found'      => __( 'No ' . $custom_post_type['name'] . ' found' ),
+					],
+					'public'       => true,
+					'show_in_rest' => true,
+					'query_var'    => true,
+					'rewrite'      => [
+						'slug' => $custom_post_type['singular'],
+					],
+					'has_archive'  => true,
+					'hierarchical' => false,
+					'menu_icon'    => $custom_post_type['menu_icon'],
+				]
+			);
+		}
 	}
-}
 
 }
